7ff5458448432728fc5bdeeaffceb99da072eeef
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-09-07 16:29:01 +0530

    [FLEXCOM] Regenerated demo for flexcom_usart_flexcom_usart_flow_control_sam_a7g5_ek
    MH3-58104
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/flexcom/usart/flexcom_usart_flow_control/firmware/src/config/sam_a7g5_ek/definitions.h
+++ apps/flexcom/usart/flexcom_usart_flow_control/firmware/src/config/sam_a7g5_ek/definitions.h
@@ -55,7 +55,6 @@
 #include "peripheral/mmu/plib_mmu.h"
 #include "peripheral/matrix/plib_matrix.h"
 #include "peripheral/pio/plib_pio.h"
-#include "peripheral/generic_timer/plib_generic_timer.h"
 #include "peripheral/flexcom/usart/plib_flexcom9_usart.h"
 
 // DOM-IGNORE-BEGIN
