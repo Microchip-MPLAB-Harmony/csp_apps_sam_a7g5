c25c51eac739fe128e840d6aaba81d9466c5f79d
Syam Nair <syam.nair@microchip.com>
2022-05-12 14:14:39 +0530

    [FLEXCOM] Regenerated demo for flexcom_spi_master_flexcom_spi_eeprom_sam_a7g5_ek
    MH3-67251
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/flexcom/spi/master/flexcom_spi_eeprom/firmware/src/packs/SAMA7G54_DFP/instance/i2smcc0.h
+++ apps/flexcom/spi/master/flexcom_spi_eeprom/firmware/src/packs/SAMA7G54_DFP/instance/i2smcc0.h
@@ -1,7 +1,7 @@
 /*
- * Instance header file for SAMA7G54
+ * Instance header file for ATSAMA7G54
  *
- * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,15 +20,15 @@
  *
  */
 
-/* file generated from device description version 2021-07-26T12:51:16Z */
+/* file generated from device description version 2022-02-07T12:32:53Z */
 #ifndef _SAMA7G_I2SMCC0_INSTANCE_
 #define _SAMA7G_I2SMCC0_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for I2SMCC0 peripheral ========== */
-#define I2SMCC0_INSTANCE_ID                      _UL_(57)   /* Inter-IC Sound Controller 0 */
-#define I2SMCC0_CLOCK_ID                         _UL_(57)   
-#define I2SMCC0_DMAC_ID_RX                       _UL_(33)   
-#define I2SMCC0_DMAC_ID_TX                       _UL_(34)   
+#define I2SMCC0_INSTANCE_ID                      (57)       /* Inter-IC Sound Controller 0 */
+#define I2SMCC0_CLOCK_ID                         (57)       
+#define I2SMCC0_DMAC_ID_RX                       (33)       
+#define I2SMCC0_DMAC_ID_TX                       (34)       
 
 #endif /* _SAMA7G_I2SMCC0_INSTANCE_ */
