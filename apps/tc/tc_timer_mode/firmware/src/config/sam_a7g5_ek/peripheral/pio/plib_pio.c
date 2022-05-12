86fdd00c24e580735876a948518c94c9dfb1d1a6
Syam Nair <syam.nair@microchip.com>
2022-05-12 14:36:23 +0530

    [TC] Regenerated demo for tc_tc_timer_mode_sam_a7g5_ek
    MH3-67271
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/tc/tc_timer_mode/firmware/src/config/sam_a7g5_ek/peripheral/pio/plib_pio.c
+++ apps/tc/tc_timer_mode/firmware/src/config/sam_a7g5_ek/peripheral/pio/plib_pio.c
@@ -44,6 +44,8 @@
 #include "plib_pio.h"
 #include "interrupts.h"
 
+
+
 /******************************************************************************
   Function:
     PIO_Initialize ( void )
@@ -59,11 +61,11 @@ void PIO_Initialize ( void )
  /* Port A Peripheral function GPIO configuration */
 	PIOA_REGS->PIO_MSKR = 0x2000U;
 	PIOA_REGS->PIO_CFGR = 0x0U;
-	
+
  /* Port A Pin 13 configuration */
 	PIOA_REGS->PIO_MSKR = 0x2000U;
 	PIOA_REGS->PIO_CFGR = (PIOA_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
-	
+
  /* Port A Latch configuration */
 	PIOA_REGS->PIO_CODR = 0x2000U;
 
@@ -72,11 +74,11 @@ void PIO_Initialize ( void )
  /* Port D Peripheral function GPIO configuration */
 	PIOD_REGS->PIO_MSKR = 0x100000U;
 	PIOD_REGS->PIO_CFGR = 0x0U;
-	
+
  /* Port D Pin 20 configuration */
 	PIOD_REGS->PIO_MSKR = 0x100000U;
 	PIOD_REGS->PIO_CFGR = (PIOD_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
-	
+
  /* Port D Latch configuration */
 	PIOD_REGS->PIO_CODR = 0x100000U;
 
@@ -119,6 +121,7 @@ uint32_t PIO_PortRead(PIO_PORT port)
     return PIO_REGS->PIO_GROUP[port].PIO_PDSR;
 }
 
+
 // *****************************************************************************
 /* Function:
     void PIO_PortWrite (PIO_PORT port, uint32_t mask, uint32_t value);
@@ -135,6 +138,7 @@ void PIO_PortWrite(PIO_PORT port, uint32_t mask, uint32_t value)
     PIO_REGS->PIO_GROUP[port].PIO_ODSR = value;
 }
 
+
 // *****************************************************************************
 /* Function:
     uint32_t PIO_PortLatchRead ( PIO_PORT port )
@@ -149,6 +153,7 @@ uint32_t PIO_PortLatchRead(PIO_PORT port)
 {
     return PIO_REGS->PIO_GROUP[port].PIO_ODSR;
 }
+
 
 // *****************************************************************************
 /* Function:
@@ -210,7 +215,7 @@ void PIO_PortToggle(PIO_PORT port, uint32_t mask)
 void PIO_PortInputEnable(PIO_PORT port, uint32_t mask)
 {
     PIO_REGS->PIO_GROUP[port].PIO_MSKR = mask;
-    PIO_REGS->PIO_GROUP[port].PIO_CFGR &= ~PIO_CFGR_DIR_Msk;	
+    PIO_REGS->PIO_GROUP[port].PIO_CFGR &= ~PIO_CFGR_DIR_Msk;
 }
 
 // *****************************************************************************
--- apps/tc/tc_timer_mode/firmware/src/packs/SAMA7G54_DFP/instance/flexcom11.h
+++ apps/tc/tc_timer_mode/firmware/src/packs/SAMA7G54_DFP/instance/flexcom11.h
@@ -1,7 +1,7 @@
 /*
- * Instance header file for SAMA7G54
+ * Instance header file for ATSAMA7G54
  *
- * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,18 +20,18 @@
  *
  */
 
-/* file generated from device description version 2021-07-26T12:51:16Z */
+/* file generated from device description version 2022-02-07T12:32:53Z */
 #ifndef _SAMA7G_FLEXCOM11_INSTANCE_
 #define _SAMA7G_FLEXCOM11_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for FLEXCOM11 peripheral ========== */
-#define FLEXCOM11_INSTANCE_ID                    _UL_(49)   /* Flexcom 11 */
-#define FLEXCOM11_CLOCK_ID                       _UL_(49)   
-#define FLEXCOM11_DMAC_ID_RX                     _UL_(27)   
-#define FLEXCOM11_DMAC_ID_TX                     _UL_(28)   
-#define FLEXCOM11_USART_FIFO_SIZE                _UL_(32)   
-#define FLEXCOM11_SPI_FIFO_SIZE                  _UL_(32)   
-#define FLEXCOM11_TWI_FIFO_SIZE                  _UL_(32)   
+#define FLEXCOM11_INSTANCE_ID                    (49)       /* Flexcom 11 */
+#define FLEXCOM11_CLOCK_ID                       (49)       
+#define FLEXCOM11_DMAC_ID_RX                     (27)       
+#define FLEXCOM11_DMAC_ID_TX                     (28)       
+#define FLEXCOM11_USART_FIFO_SIZE                (32)       
+#define FLEXCOM11_SPI_FIFO_SIZE                  (32)       
+#define FLEXCOM11_TWI_FIFO_SIZE                  (32)       
 
 #endif /* _SAMA7G_FLEXCOM11_INSTANCE_ */
