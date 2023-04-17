7e7d45a1d67b9efc8c0fe9768932811d10e3d27b
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-04-17 18:45:56 +0530

    [PIO] Regenerated demo for pio_pio_led_on_off_interrupt_sam_a7g5_ek
    MH3-78705

--- apps/pio/pio_led_on_off_interrupt/firmware/src/config/sam_a7g5_ek/initialization.c
+++ apps/pio/pio_led_on_off_interrupt/firmware/src/config/sam_a7g5_ek/initialization.c
@@ -104,6 +104,8 @@
 
 void SYS_Initialize ( void* data )
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
   
     DWDT_Initialize();
@@ -121,7 +123,7 @@ void SYS_Initialize ( void* data )
 
 
 
-
+    /* MISRAC 2012 deviation block end */
 }
 
 
